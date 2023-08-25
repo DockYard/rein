01cb99c8911bf7026e658416c67b0b3c88618cba
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-08-25 00:40:06 -0300

    docs: document agent

--- lib/rein/agent.ex
+++ lib/rein/agent.ex
@@ -5,17 +5,23 @@ defmodule Rein.Agent do
 
   @typedoc "An arbitrary `Nx.Container` that holds metadata for the agent"
   @type t :: Nx.Container.t()
+  @typedoc "The full state of the current Reinforcement Learning process, as stored in the `Rein` struct"
   @type rl_state :: Rein.t()
 
   @doc """
   Initializes the agent state with the given agent-specific options.
 
-  Also calls `c:reset/2` in the end.
+  Should be implemented in a way that the result would be semantically
+  the same as if `c:reset/2` was called in the end of the function.
+
+  As a suggestion, only initialize fixed values here, and delegate the
+  rest to `c:reset/2` internally.
   """
   @callback init(random_key :: Nx.t(), opts :: keyword) :: {t(), random_key :: Nx.t()}
 
   @doc """
-  Resets any values that aren't fixed for the agent state.
+  Resets any values that vary between sessions (which would be episodes
+  for episodic tasks) for the agent state.
   """
   @callback reset(random_key :: Nx.t(), rl_state :: t) :: {t(), random_key :: Nx.t()}
 
