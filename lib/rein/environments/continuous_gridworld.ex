e182ccb1f21815387002310797b4f424f1752145
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-10-31 09:27:41 -0300

    feat: add continuous gridworld

---
+++ lib/rein/environments/continuous_gridworld.ex
@@ -0,0 +1,185 @@
+defmodule Rein.Environments.ContinuousGridworld do
+  @moduledoc """
+  Gridworld-like environment with 1 continuous action.
+
+  The agent starts at a random position in a 5x5 grid.
+  Unlike normal gridworld where the agent can move freely in 4 directions,
+  this environment has 1 continuous action that turns the agent left or right.
+  The agent will always move after turning with a continuous speed of 0.1, and each
+  iteration comprises a $\\delta t$ of 1.0, so the agent will move 0.1 units per iteration.
+  """
+  import Nx.Defn
+
+  @behaviour Rein.Environment
+
+  @derive {Nx.Container,
+           containers: [
+             :x,
+             :y,
+             :direction,
+             :target_x,
+             :target_y,
+             :reward,
+             :is_terminal,
+             :possible_targets,
+             :has_reached_target,
+             :remaining_seconds
+           ],
+           keep: []}
+  defstruct [
+    :x,
+    :y,
+    :direction,
+    :target_x,
+    :target_y,
+    :reward,
+    :is_terminal,
+    :possible_targets,
+    :has_reached_target,
+    :remaining_seconds
+  ]
+
+  @min_x 0
+  @max_x 5
+  @min_y 0
+  @max_y 5
+
+  @speed 0.1
+  @dt 1.0
+
+  def bounding_box, do: {@min_x, @max_x, @min_y, @max_y}
+
+  # x, y, direction, target_x, target_y, has_reached_target, distance_norm
+  @doc "The size of the state vector returned by `as_state_vector/1`"
+  def state_vector_size, do: 7
+
+  # left or right
+  def num_actions, do: 1
+
+  @impl true
+  def init(random_key, opts) do
+    opts = Keyword.validate!(opts, [:possible_targets])
+
+    possible_targets =
+      opts[:possible_targets] || raise ArgumentError, "missing option :possible_targets"
+
+    reset(random_key, %__MODULE__{possible_targets: possible_targets})
+  end
+
+  @impl true
+  def reset(random_key, %__MODULE__{} = state) do
+    {x, random_key} = Nx.Random.uniform(random_key, @min_x, @max_x)
+
+    # possible_targets is a {n, 2} tensor that contains targets that we want to sample from
+    # this is so we avoid retraining every episode on the same target, which can lead to
+    # overfitting
+    {target, random_key} =
+      Nx.Random.choice(random_key, state.possible_targets, samples: 1, axis: 0)
+
+    target = Nx.reshape(target, {2})
+
+    zero = Nx.tensor(0, type: :f32)
+
+    [x, y, target_x, target_y, zero_bool, _key] =
+      Nx.broadcast_vectors([x, zero, target[0], target[1], random_key, Nx.u8(0)])
+
+    state = %{
+      state
+      | x: x,
+        y: y,
+        target_x: target_x,
+        target_y: target_y,
+        direction: zero,
+        reward: zero,
+        is_terminal: zero_bool,
+        has_reached_target: zero_bool,
+        remaining_seconds: zero
+    }
+
+    {state, random_key}
+  end
+
+  @impl true
+  defn apply_action(state, action) do
+    %{x: x, y: y, direction: direction} = env = state.environment_state
+
+    dtheta = action * Nx.Constants.pi()
+
+    new_x = x + @speed * Nx.cos(dtheta) * @dt
+    new_y = y + @speed * Nx.sin(dtheta) * @dt
+
+    new_direction = wrap_phase(direction + dtheta)
+
+    new_env = %{
+      env
+      | x: Nx.clip(new_x, @min_x, @max_x),
+        y: Nx.clip(new_y, @min_y, @max_y),
+        direction: new_direction,
+        remaining_seconds: env.remaining_seconds - @dt
+    }
+
+    updated_env =
+      new_env
+      |> is_terminal_state()
+      |> calculate_reward()
+
+    %{state | environment_state: updated_env}
+  end
+
+  defnp wrap_phase(theta) do
+    two_pi = 2 * Nx.Constants.pi()
+    Nx.remainder(Nx.remainder(theta, two_pi) + two_pi, two_pi)
+  end
+
+  defnp calculate_reward(env) do
+    distance = Nx.abs(env.target_x - env.x) + Nx.abs(env.target_y - env.y)
+    reward = -1.0 * distance
+
+    %{env | reward: reward}
+  end
+
+  defnp is_terminal_state(env) do
+    has_reached_target = has_reached_target(env)
+    out_of_bounds = env.x < @min_x or env.x > @max_x or env.y < @min_y or env.y > @max_y
+
+    is_terminal = has_reached_target or out_of_bounds or env.remaining_seconds < 1
+
+    %__MODULE__{env | is_terminal: is_terminal, has_reached_target: has_reached_target}
+  end
+
+  defnp has_reached_target(%__MODULE__{x: x, y: y, target_x: target_x, target_y: target_y}) do
+    (target_x - x) ** 2 + (target_y - y) ** 2 <= 1
+  end
+
+  defnp normalize(v, min, max), do: (v - min) / (max - min)
+
+  @doc """
+  Default function for turning the environment into a vector representation.
+  """
+  defn as_state_vector(%{
+         x: x,
+         y: y,
+         target_x: target_x,
+         target_y: target_y,
+         has_reached_target: has_reached_target
+       }) do
+    x = normalize(x, @min_x, @max_x)
+    y = normalize(y, @min_y, @max_y)
+
+    target_x = normalize(target_x, @min_x, @max_x)
+    target_y = normalize(target_y, @min_y, @max_y)
+
+    # max distance is sqrt(1 ** 2 + 1 ** 2) = sqrt(2)
+    distance_norm = Nx.sqrt((x - target_x) ** 2 + (y - target_y) ** 2) / Nx.sqrt(2)
+
+    Nx.stack([
+      x,
+      y,
+      target_x,
+      target_y,
+      has_reached_target,
+      distance_norm
+    ])
+    |> Nx.new_axis(0)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -74,6 +74,7 @@ defmodule Rein.MixProject do
       extras: [
         "guides/gridworld.livemd"
       ],
+      before_closing_body_tag: &before_closing_body_tag/1,
       groups_for_functions: [],
       groups_for_modules: [
         Agents: [
@@ -92,4 +93,25 @@ defmodule Rein.MixProject do
       ]
     ]
   end
+
+  defp before_closing_body_tag(:html) do
+    """
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/katex.min.css" integrity="sha384-beuqjL2bw+6DBM2eOpr5+Xlw+jiH44vMdVQwKxV28xxpoInPHTVmSvvvoPq9RdSh" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/katex.min.js" integrity="sha384-aaNb715UK1HuP4rjZxyzph+dVss/5Nx3mLImBe9b0EW4vMUkc1Guw4VRyQKBC0eG" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/contrib/auto-render.min.js" integrity="sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR" crossorigin="anonymous"
+            onload="renderMathInElement(document.body);"></script>
+    <script>
+      document.addEventListener("DOMContentLoaded", function() {
+        renderMathInElement(document.body, {
+          delimiters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
+          ]
+        });
+      });
+    </script>
+    """
+  end
+
+  defp before_closing_body_tag(_), do: ""
 end
